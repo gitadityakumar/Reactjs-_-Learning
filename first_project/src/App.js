008f0ffcd0968915844075f3902bfcefe9b4047e
gitadityakumar <iamadityakumar05@proton.me>
2023-07-20 17:50:06 +0530

    applied the concept of props

--- first_project/src/App.js
+++ first_project/src/App.js
@@ -1,15 +1,55 @@
 import ExpenseItem from "./Components/ExpenseItem";
 
-
-
-function App(){
+function App() {
+  let expenses = [
+    {
+      id: "E1",
+      title: "Schoole Fee",
+      amount: "250",
+      date: new Date(2023, 7, 21),
+    },
+    {
+      id: "E2",
+      title: "Books",
+      amount: "230",
+      date: new Date(2023, 4, 21),
+    },
+    {
+      id: "E3",
+      title: "House Rent",
+      amount: "800",
+      date: new Date(2023, 7, 18),
+    },
+    {
+      id: "E4",
+      title: "Food",
+      amount: "545",
+      date: new Date(2023, 7, 8),
+    },
+  ];
   return (
     <div>
-      
-      <h2>
-        Lets get started
-      </h2>
-      <ExpenseItem></ExpenseItem>
+      <h2>Lets get started</h2>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      ></ExpenseItem>
     </div>
   );
 }
--- first_project/src/Components/ExpenseItem.js
+++ first_project/src/Components/ExpenseItem.js
@@ -1,19 +1,23 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    return (
-        <div className = "expense-item">
-            <div>
-               July 18th 2023 
-            </div>
-            <div className ="expense-item_description"> 
-                <h2>Car Insurence</h2>
-                <div className="epense-item__price">$200</div>
-            </div>
-        </div>
-    );
-
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US',{month:'long'});
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString('en-US',{day:'2-digit'});
+  //props is a object
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
+        <div className="epense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
 }
-
 
 export default ExpenseItem;
