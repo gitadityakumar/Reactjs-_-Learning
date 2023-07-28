92f161146ac2d7946bce24363422124d679defbc
gitadityakumar <iamadityakumar05@proton.me>
2023-07-28 18:35:28 +0530

    added css of Expenses.css

--- first_project/src/App.js
+++ first_project/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
 
 function App() {
   let expenses = [
@@ -30,26 +30,7 @@ function App() {
   return (
     <div>
       <h2>Lets get started</h2>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-      ></ExpenseItem>
+      <Expenses item={expenses}/>
     </div>
   );
 }
