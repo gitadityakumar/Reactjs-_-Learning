5433dd0b460284cce1b377196e1532dc78639cac
gitadityakumar <iamadityakumar05@proton.me>
2023-07-28 18:50:24 +0530

    improved some css

--- first_project/src/Components/Expenses.js
+++ first_project/src/Components/Expenses.js
@@ -9,22 +9,22 @@ function Expense(props){
         date={props.item[0].date}
         title={props.item[0].title}
         amount={props.item[0].amount}
-        ></ExpenseItem>
+        />
         <ExpenseItem
         date={props.item[1].date}
         title={props.item[1].title}
         amount={props.item[1].amount}
-        ></ExpenseItem>
+        />
         <ExpenseItem
         date={props.item[2].date}
         title={props.item[2].title}
         amount={props.item[2].amount}
-        ></ExpenseItem>
+        />
         <ExpenseItem
         date={props.item[3].date}
         title={props.item[3].title}
         amount={props.item[3].amount}
-        ></ExpenseItem>
+        />
         </div>
     );
 
