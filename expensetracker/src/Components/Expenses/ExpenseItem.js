94f2ad2509e980ca9e8dad3fed29677a00612afb
gitadityakumar <ITSADITYASINGH05@GMAIL.COM>
2023-08-08 18:21:44 +0530

    Handling input in reactjs

--- expensetracker/src/Components/Expenses/ExpenseItem.js
+++ expensetracker/src/Components/Expenses/ExpenseItem.js
@@ -6,10 +6,16 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) =>{
 
+  const [newTitle , setNewTitle] = useState(" ");
+
    const [title , setTitle]= useState(props.title);
 
    const clickHandler = () => {
-     setTitle('New Title..');
+     setTitle(newTitle);
+   }
+
+   const changeHandler = (event) => {
+     setNewTitle(event.target.value);
    }
 
   //props is a object
@@ -21,7 +27,9 @@ const ExpenseItem = (props) =>{
         <h2>{title}</h2>
         <div className="epense-item__price">{props.amount}</div>
       </div>
+      <input type='text' value={newTitle} onChange={changeHandler} />
       <button onClick={clickHandler}>Change Title</button>
+
     </Card>
   );
 }
