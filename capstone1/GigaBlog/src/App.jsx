b5325149e79f6efd8e75e4ca5f7eed8ebb7c4a3c
gitadityakumar <ITSADITYASINGH05@GMAIL.COM>
2023-09-26 16:37:33 +0530

    ENV and Appwrite in React project

---
+++ capstone1/GigaBlog/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import config from './config/config'
+
+function App() {
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(config.appwriteUrl)
+  
+
+  return (
+    <>
+      <h1>Giga-Blog</h1>
+    </>
+  )
+}
+
+export default App
